cb6e932dd5211c8db0170abcba602b375df5966b
José Argenis García Valderrama <a25joseagv@teis.intranet>
2025-09-25 20:23:35 +0200

    ejercicios arrays hasta el 3, listos

---
+++ .idea/25-09-25-arrays/src/Main.java
@@ -0,0 +1,77 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        1. La NASA está probando sensores de temperatura en las alas de un avión experimental.
+//        Crea un array de enteros que represente las temperaturas registradas en cada ala. Recorre el
+//        array y muestra únicamente las temperaturas que superan los 100 grados.
+
+//          int[] recordedTemperatures = {70, 80, 101, 92, 500, 20, 201, 199};
+//
+//          for(int i = 0; i < recordedTemperatures.length; i++){
+//              if(recordedTemperatures[i] > 100) {
+//                  System.out.println("Temperatura: " + recordedTemperatures[i]);
+//              }
+//          }
+
+//        2. Un streamer guarda la cantidad de espectadores en cada hora de transmisión en un array.
+//                Recorre el array e imprime en qué horas el número de espectadores fue mayor a 1000.
+//        Además, cuenta cuántas veces se superó esa cifra.
+
+//         int[] spectatorsPerHour = {20, 522, 1200, 900, 1300, 1800, 200, 400, 1020};
+//         int maxSpectatorsCountSuperate = 0;
+//         for(int i = 0; i < spectatorsPerHour.length; i++){
+//             if(spectatorsPerHour[i] > 1000){
+//                 System.out.println("Se superaron los 1000 espectaroes en la hora: " + (i + 1));
+//                 maxSpectatorsCountSuperate++;
+//             }
+//         }
+//
+//        System.out.println("Se superaron " + maxSpectatorsCountSuperate + " los 1000 expectadores");
+
+//        3. Un banco registra en un array los movimientos de una cuenta (positivos ingresos,
+//                negativos gastos). Recorre el array y calcula el saldo final. Muestra también cuántos
+//        movimientos fueron ingresos y cuántos fueron gastos.
+
+//          double userMoneyCount = 1000.0;
+//          int movimientosPositivos = 0;
+//          int movimientosNegativos = 0;
+//
+//          double[] movimientosCuenta = {125.0, -20.5, 200.0, 500.0, 20.0, -300.0, -100.0, 325.0, 200.0, -700.1};
+//
+//          for(int i = 0; i < movimientosCuenta.length; i++){
+//              if(movimientosCuenta[i] > 0){
+//                  movimientosPositivos++;
+//              } else if (movimientosCuenta[i] < 0){
+//                  movimientosNegativos++;
+//              }
+//              userMoneyCount += movimientosCuenta[i];
+//          }
+//
+//        System.out.println("Total de cuenta: " + userMoneyCount);
+//        System.out.println("Movimientos positivos: " + movimientosPositivos);
+//        System.out.println("Movimientos negativos: " + movimientosNegativos);
+
+//        4. Un profesor guarda las notas de 20 alumnos en un array. Recorre el array y determina
+//        cuántos alumnos han aprobado (nota >= 5). Crea una función que reciba una nota y
+//        devuelva si es aprobado o suspenso.
+
+
+
+//        5. Tienes un array con el tiempo de ejecución (en milisegundos) de varios algoritmos.
+//                Recorre el array y encuentra cuál fue el tiempo más rápido y el más lento.
+//        6. En un partido de baloncesto, un array almacena los puntos anotados por cada jugador de
+//        un equipo. Recorre el array y muestra el total de puntos del equipo. Además calcula la media
+//        de puntos por jugador.
+//        7. Un satélite envía un array con niveles de radiación detectados en distintas órbitas.
+//                Recorre el array y genera un nuevo array solo con los valores superiores a 50. Imprime el
+//        array resultante.
+//        8. Convierte el String “La verdad es que estos ejercicios me cuecen" en un array de chars y,
+//        usando StringBuilder, construye un nuevo String solo con las vocales. Muestra el resultado.
+//        9. Un número de cuenta bancaria se guarda en un String. Convierte el String a char[],
+//        recórrelo y con StringBuilder genera un nuevo String que solo contenga los dígitos pares.
+//        10. En un array se registran las velocidades de los corredores en una maratón. Recorre el
+//        array y muestra qué corredores superaron los 20 km/h. Crea una función que reciba la
+//        velocidad y devuelva un mensaje indicando si es 'Rápido' o 'Normal'.
+    }
+}
